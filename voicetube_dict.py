f43379242f3935de331b8c39372a2a376ae2f3e2
BensonBair <bensonb1108@gmail.com>
2018-05-28 13:41:25 +0800

    Add files via upload

---
+++ voicetube_dict.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 17 23:47:56 2017
+
+@author: user
+"""
+
+import requests
+import bs4
+import pandas as pd
+
+# create empty cv file
+#temp = pd.DataFrame(columns = ['Vocabulary', 'Definition', 'Example', 'importance'])
+#temp.to_csv('C:\\Users\\user\\Documents\\Output_Files\\vocabulary_lookup.csv', encoding='utf-8', index = False)
+
+# Program 
+print('================================\n=VoiceTube Dictionary by Benson=\n================================\n')
+print("Enter 'q' to quit\n")
+file = pd.read_csv('C:\\Users\\user\\Documents\\Output_Files\\vocabulary.csv', encoding='utf-8')
+
+# while loop
+while True:
+    # enter vocabulary
+    word = input('\nEnter a vocabulary:')    
+    # enter q to quit the program
+    if word == 'q':
+        print('Thank you!')
+        break
+    else:
+        path = 'https://tw.voicetube.com/definition/' + word
+        # look up the dictionary
+        try:
+            # get request with user agent
+            r = requests.get(path, headers={'user-agent' : 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'})
+            r.encoding = 'utf-8'
+            soup = bs4.BeautifulSoup(r.text, 'lxml')
+            meta = soup.find_all('meta')
+            # word info happens to be in forth line 
+            content = str(meta[4])
+            # definition & example of the word
+            definition = content[content.find('：')+1:content.find('例句')].replace('； ', '/ ').replace('amp; ', '')[:-2]
+            example = content[content.find('例句')+3:content.find('。')+1]
+            # print out the results
+            print('Definition:\n' + definition)
+            print('Example:\n' + example)
+            # check if the work has been looked up bef
+            if word in file.Vocabulary.values:
+                print('The word is in row ' + str(file.loc[file.Vocabulary == word].index.values[0]))
+            else:
+                save = input('\nEnter "Y" to save:')
+                if save == 'Y':
+                    importance = input('\nEnter level of importance(1-3):')
+                    file.loc[len(file)] = [word, definition, example, pd.to_numeric(importance)]
+                    file.to_csv('C:\\Users\\user\\Documents\\Output_Files\\vocabulary.csv', encoding='utf-8', index=False)
+        # check if spelling mistakes exist
+        except IndexError:
+            print('Word not found. Please enter again.')
+    
+
+
+
+
+
+
+
